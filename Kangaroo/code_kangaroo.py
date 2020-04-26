644ae032c0cb0b97244ea42753ee88d5a300c256
Mehran Kader <kmehran.1106@gmail.com>
2020-04-26 12:44:46 +0600

    add: problem Kangaroo

---
+++ Kangaroo/code_kangaroo.py
@@ -0,0 +1,36 @@
+import pytest
+
+
+def kangaroo(x1: int, v1: int, x2: int, v2: int) -> bool:
+    if x1 < x2 and v1 <= v2:
+        return False
+    else:
+        n1 = (x2-x1)/(v1-v2)
+        n2 = (x2-x1)//(v1-v2)
+
+        if n1 == n2:
+            return True
+        else:
+            return False
+
+
+@pytest.fixture
+def get_fixtures():
+    first_input = [28, 8, 96, 2]
+    first_output = False
+
+    second_input = [0, 3, 4, 2]
+    second_output = True
+
+    second_input = [43, 2, 70, 2]
+    second_output = True
+
+    return [
+        (first_input, first_output),
+        (second_input, second_output)
+    ]
+
+
+def test_code(get_fixtures):
+    for data in get_fixtures:
+        assert kangaroo(*data[0]) == data[1]
